cef1194517b57a8fd411a07afd69e2c9c7fa0eb7
Yauheni Akhotnikau <eao197@gmail.com>
2015-02-08 09:56:16 +0000

    Description of main features of version 5.5.3 is added to doxygen-relaten stuff.
    --HG--
    branch : 5.5

--- dev/so_5/rt/h/subscription_storage_fwd.hpp
+++ dev/so_5/rt/h/subscription_storage_fwd.hpp
@@ -48,6 +48,15 @@ using subscription_storage_factory_t =
 /*!
  * \since v.5.5.3
  * \brief Factory for default subscription storage object.
+ *
+ * \note Creates adaptive storage with vector-based storage for
+ * small amount of subscriptions and map-based storage for
+ * large amount of subscriptions.
+ *
+ * \par More about subscription storage tuning
+ * See \ref so_5_5_3__subscr_storage_selection for more details about selection
+ * of appropriate subscription storage type.
+ *
  */
 SO_5_FUNC subscription_storage_factory_t
 default_subscription_storage_factory();
@@ -55,6 +64,15 @@ default_subscription_storage_factory();
 /*!
  * \since v.5.5.3
  * \brief Factory for default subscription storage based on std::unordered_map.
+ *
+ * \note This storage is efficient only in the cases of very large amount
+ * of subscriptions (from several hundreds to thousands). For smaller amounts
+ * map-based and vector-based storages are more appropriate.
+ *
+ * \par More about subscription storage tuning
+ * See \ref so_5_5_3__subscr_storage_selection for more details about selection
+ * of appropriate subscription storage type.
+ *
  */
 SO_5_FUNC subscription_storage_factory_t
 hash_table_based_subscription_storage_factory();
@@ -62,6 +80,16 @@ hash_table_based_subscription_storage_factory();
 /*!
  * \since v.5.5.3
  * \brief Factory for subscription storage based on std::vector.
+ *
+ * \note Uses very simple working scheme: all subscriptions are stored in the
+ * linear vector and simpliest linear search is used for seaching and
+ * manipulating of subscriptions. Because of that vector-based storage must be
+ * used only with very small amount of subscriptions.
+ *
+ * \par More about subscription storage tuning
+ * See \ref so_5_5_3__subscr_storage_selection for more details about selection
+ * of appropriate subscription storage type.
+ *
  */
 SO_5_FUNC subscription_storage_factory_t
 vector_based_subscription_storage_factory(
@@ -71,6 +99,15 @@ vector_based_subscription_storage_factory(
 /*!
  * \since v.5.5.3
  * \brief Factory for subscription storage based on std::map.
+ *
+ * \note Uses std::map as an underlying storage. And very efficient when count
+ * of subscription in measured in dozens. For very large amount of
+ * subscriptions the hash-table-based storage is more efficient.
+ *
+ * \par More about subscription storage tuning
+ * See \ref so_5_5_3__subscr_storage_selection for more details about selection
+ * of appropriate subscription storage type.
+ *
  */
 SO_5_FUNC subscription_storage_factory_t
 map_based_subscription_storage_factory();
@@ -79,9 +116,15 @@ map_based_subscription_storage_factory();
  * \since v.5.5.3
  * \brief Factory for adaptive subscription storage.
  *
+ * \par Description
  * This storage will use vector-based storage for small amount
  * of subscriptions. And map-based storage for large amount
  * of subscriptions.
+ *
+ * \par More about subscription storage tuning
+ * See \ref so_5_5_3__subscr_storage_selection for more details about selection
+ * of appropriate subscription storage type.
+ *
  */
 SO_5_FUNC subscription_storage_factory_t
 adaptive_subscription_storage_factory(
@@ -94,9 +137,38 @@ adaptive_subscription_storage_factory(
  * \since v.5.5.3
  * \brief Factory for adaptive subscription storage.
  *
+ * \par Description
  * This storage will use storage created by \a small_storage_factory for
  * small amount of subscriptions. And storage created by
  * \a large_storage_factory for large amount of subscriptions.
+ *
+ * \par Cascading of storages
+ * This function can be used for creating more complex cascades of
+ * several subscription storages. For example, the following code creates
+ * adaptive storage with three underlying objects. The first will be used
+ * for 0..10 subscriptions. The seconds will be used for 11..100 subscriptions.
+ * And the third for 100+ subscriptions:
+\code
+so_5::rt::adaptive_subscription_storage_factory(
+	// First threshold for switching from the first storage to the second.
+	10,
+	// First storage -- simple vector-based.
+	so_5::rt::vector_based_subscription_storage_factory(10),
+	// There will be another adaptive storage.
+	so_5::rt::adaptive_subscription_storage_factory(
+		// Second threshold for switching from the second storage to the third.
+		100,
+		// Second storage.
+		so_5::rt::map_based_subscription_storage_factory(),
+		// Third storage.
+		so_5::rt::hash_table_based_subscription_storage_factory() ) );
+\endcode
+ *
+ *
+ * \par More about subscription storage tuning
+ * See \ref so_5_5_3__subscr_storage_selection for more details about selection
+ * of appropriate subscription storage type.
+ *
  */
 SO_5_FUNC subscription_storage_factory_t
 adaptive_subscription_storage_factory(
