6ecf4b14372380cc255bc0136db5afc31b67a6c2
Yauheni Akhotnikau <eao197@gmail.com>
2017-09-19 11:45:38 +0000

    Version number changed to 5.5.19.4.
    --HG--
    branch : 5.5

--- dev/so_5/h/version.hpp
+++ dev/so_5/h/version.hpp
@@ -12,7 +12,7 @@
 
 #pragma once
 
-// The current version is 5.5.19.3
+// The current version is 5.5.19.4
 //
 /*!
  * A number next to generation number in SObjectizer's version.
@@ -43,7 +43,7 @@
  * or API fixes are added to SObjectizer but SObjectizer API is compatible on
  * source-code level.
  */
-#define SO_5_VERSION_PATCH 3ull
+#define SO_5_VERSION_PATCH 4ull
 
 /*!
  * Helper macro for make single number representation of SObjectizer's version.
--- dev/so_5/version.rb
+++ dev/so_5/version.rb
@@ -1,3 +1,3 @@
 module So5
-	VERSION = "5.5.19.3"
+	VERSION = "5.5.19.4"
 end
