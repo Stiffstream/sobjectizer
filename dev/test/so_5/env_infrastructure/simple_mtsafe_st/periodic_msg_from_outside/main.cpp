9003753d36994487243e2a8f634cf4d51969200b
Yauheni Akhotnikau <eao197@gmail.com>
2019-02-16 16:27:23 +0300

    Fix warning from clang compiler.
    --HG--
    branch : mbox-refactor

--- dev/test/so_5/env_infrastructure/simple_mtsafe_st/periodic_msg_from_outside/main.cpp
+++ dev/test/so_5/env_infrastructure/simple_mtsafe_st/periodic_msg_from_outside/main.cpp
@@ -44,7 +44,7 @@ main()
 							test_mbox = coop.make_agent< a_test_t >()->so_direct_mbox();
 						} );
 
-						outside_thread = thread( [&env, test_mbox] {
+						outside_thread = thread( [test_mbox] {
 							this_thread::sleep_for( chrono::milliseconds( 350 ) );
 							auto timer = so_5::send_periodic< a_test_t::tick >(
 									test_mbox,
