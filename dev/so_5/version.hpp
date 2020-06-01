83e116df4e6e0a4467905c3ad86611eaa2a99b9f
Yauheni Akhotnikau <eao197@gmail.com>
2020-06-01 08:15:12 +0300

    Version number updated to 5.7.1.

--- dev/so_5/version.hpp
+++ dev/so_5/version.hpp
@@ -12,7 +12,7 @@
 
 #pragma once
 
-// The current version is 5.7.0.1
+// The current version is 5.7.1
 //
 /*!
  * A number next to generation number in SObjectizer's version.
@@ -32,7 +32,7 @@
  * When minor version is changed some new features are added to SObjectizer
  * but SObjectizer API is compatible on source-code level.
  */
-#define SO_5_VERSION_MINOR 0ull
+#define SO_5_VERSION_MINOR 1ull
 
 /*!
  * A number next to minor version in SObjectizer's version.
@@ -43,7 +43,7 @@
  * or API fixes are added to SObjectizer but SObjectizer API is compatible on
  * source-code level.
  */
-#define SO_5_VERSION_PATCH 1ull
+#define SO_5_VERSION_PATCH 0ull
 
 /*!
  * Helper macro for make single number representation of SObjectizer's version.
--- dev/so_5/version.rb
+++ dev/so_5/version.rb
@@ -1,3 +1,3 @@
 module So5
-  VERSION = "5.7.0.1"
+  VERSION = "5.7.1"
 end
