04ec6fcf63eb977e9f45348b7c05b0d98c8f0b60
Yauheni Akhotnikau <eao197@gmail.com>
2020-12-17 15:29:27 +0300

    Version number is changed to 5.7.2.1.

--- dev/so_5/cmake/target.cmake
+++ dev/so_5/cmake/target.cmake
@@ -1,3 +1,3 @@
-set(SO_5_VERSION "5.7.2")
+set(SO_5_VERSION "5.7.2.1")
 set(SO_5_SHARED_LIB so.${SO_5_VERSION})
 set(SO_5_STATIC_LIB so_s.${SO_5_VERSION})
--- dev/so_5/version.rb
+++ dev/so_5/version.rb
@@ -1,3 +1,3 @@
 module So5
-  VERSION = "5.7.2"
+  VERSION = "5.7.2.1"
 end
