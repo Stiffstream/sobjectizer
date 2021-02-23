83a547a101d0484a0b5e9fd5f3f77ec529517d0b
Yauheni Akhotnikau <eao197@gmail.com>
2021-02-23 12:10:02 +0300

    New CI pipeline.
    Config for Travis CI removed.
    Config for GitHub Actions for 5.7-dev branch added.

--- dev/cmake/run_tests.bat
+++ dev/cmake/run_tests.bat
@@ -1,5 +1,11 @@
+@echo off
 setlocal
-set PATH=%PATH%;${CMAKE_INSTALL_PREFIX}\bin
-cmd /c ctest --force-new-ctest-process
+
+if --%1 == -- (set SO5_TARGET_PATH=target) else (set SO5_TARGET_PATH=%1)
+if --%2 == -- (set SO5_BUILD_MODE=Release) else (set SO5_BUILD_MODE=%2)
+
+set PATH=%PATH%;%~dp0\%SO5_TARGET_PATH%\bin
+cmd /c ctest -C %SO5_BUILD_MODE% --force-new-ctest-process
 if %errorlevel% neq 0 exit /b 1
+
 endlocal
