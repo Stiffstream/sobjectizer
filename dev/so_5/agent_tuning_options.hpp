52578fe9fc174130be00850b3d8e63a795ccb324
Yauheni Akhotnikau <eao197@gmail.com>
2022-12-07 15:39:28 +0300

    Another FIXME resolved.

--- dev/so_5/agent_tuning_options.hpp
+++ dev/so_5/agent_tuning_options.hpp
@@ -93,10 +93,9 @@ class agent_tuning_options_t
 				return *this;
 			}
 
-		//FIXME: this method should be marked as [[nodiscard]] and
-		//noexcept in SO-5.8.
+		[[nodiscard]]
 		const subscription_storage_factory_t &
-		query_subscription_storage_factory() const
+		query_subscription_storage_factory() const noexcept
 			{
 				return m_subscription_storage_factory;
 			}
