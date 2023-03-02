c4c3a9d3227ea4a3541363259ee67d9f7e3000b2
Yauheni Akhotnikau <eao197@gmail.com>
2023-03-02 16:02:50 +0300

    A new unit-test.

--- dev/test/so_5/coop/build_tests.rb
+++ dev/test/so_5/coop/build_tests.rb
@@ -29,4 +29,6 @@ MxxRu::Cpp::composite_target {
 	required_prj( "#{path}/shutdown_while_reg/prj.ut.rb" )
 	required_prj( "#{path}/child_coop_map/prj.ut.rb" )
 	required_prj( "#{path}/issue_28/prj.ut.rb" )
+	required_prj( "#{path}/msg_loss_at_registration/prj.ut.rb" )
 }
+
