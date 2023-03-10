18955bb5815f63fa0065f67537d46d93518440e0
Yauheni Akhotnikau <eao197@gmail.com>
2023-03-10 15:40:51 +0300

    New unit-test.

--- dev/test/so_5/mbox/sink_binding/build_tests.rb
+++ dev/test/so_5/mbox/sink_binding/build_tests.rb
@@ -8,4 +8,5 @@ MxxRu::Cpp::composite_target {
 	required_prj( "#{path}/simple/prj.ut.rb" )
 	required_prj( "#{path}/simple_with_delivery_filter/prj.ut.rb" )
 	required_prj( "#{path}/single_sink_clear/prj.ut.rb" )
+	required_prj( "#{path}/single_sink_too_deep/prj.ut.rb" )
 }
