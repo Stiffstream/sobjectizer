3ddb5a7400105b68afad214c876151f8292e0f84
Yauheni Akhotnikau <eao197@gmail.com>
2023-05-22 16:07:10 +0300

    BugFix.
    The m_default_disp_params was forgotten in environment_params_t's
    swap(), move constructor and operator.

--- dev/test/so_5/environment/moveable_params/main.cpp
+++ dev/test/so_5/environment/moveable_params/main.cpp
@@ -19,10 +19,61 @@ UT_UNIT_TEST( environment_params )
 	so_5::environment_params_t param = make_param();
 }
 
+UT_UNIT_TEST( default_disp_params_on_move_ctor )
+{
+	so_5::environment_params_t params;
+
+	{
+		UT_CHECK_EQ( true,
+				so_5::work_thread_activity_tracking_t::unspecified ==
+						params.default_disp_params().work_thread_activity_tracking() );
+
+		params.default_disp_params(
+				so_5::disp::one_thread::disp_params_t{}
+						.turn_work_thread_activity_tracking_on() );
+
+		UT_CHECK_EQ( true,
+				so_5::work_thread_activity_tracking_t::on ==
+						params.default_disp_params().work_thread_activity_tracking() );
+	}
+
+	so_5::environment_params_t p2{ std::move(params) };
+	UT_CHECK_EQ( true,
+			so_5::work_thread_activity_tracking_t::on ==
+					p2.default_disp_params().work_thread_activity_tracking() );
+}
+
+UT_UNIT_TEST( default_disp_params_on_move_op )
+{
+	so_5::environment_params_t params;
+
+	{
+		UT_CHECK_EQ( true,
+				so_5::work_thread_activity_tracking_t::unspecified ==
+						params.default_disp_params().work_thread_activity_tracking() );
+
+		params.default_disp_params(
+				so_5::disp::one_thread::disp_params_t{}
+						.turn_work_thread_activity_tracking_on() );
+
+		UT_CHECK_EQ( true,
+				so_5::work_thread_activity_tracking_t::on ==
+						params.default_disp_params().work_thread_activity_tracking() );
+	}
+
+	so_5::environment_params_t p2;
+	p2 = std::move(params);
+	UT_CHECK_EQ( true,
+			so_5::work_thread_activity_tracking_t::on ==
+					p2.default_disp_params().work_thread_activity_tracking() );
+}
+
 int
 main()
 {
 	UT_RUN_UNIT_TEST( environment_params )
+	UT_RUN_UNIT_TEST( default_disp_params_on_move_ctor )
+	UT_RUN_UNIT_TEST( default_disp_params_on_move_op )
 
 	return 0;
 }
