acd152ab5f172fe2e45b5daac01dce7b7eb77977
Yauheni Akhotnikau <eao197@gmail.com>
2023-05-22 18:29:34 +0300

    [skip ci] Version bump.

--- dev/so_5/version.hpp
+++ dev/so_5/version.hpp
@@ -12,7 +12,7 @@
 
 #pragma once
 
-// The current version is 5.7.4.2
+// The current version is 5.7.4.3
 //
 /*!
  * A number next to generation number in SObjectizer's version.
@@ -43,7 +43,7 @@
  * or API fixes are added to SObjectizer but SObjectizer API is compatible on
  * source-code level.
  */
-#define SO_5_VERSION_PATCH 2ull
+#define SO_5_VERSION_PATCH 3ull
 
 /*!
  * Helper macro for make single number representation of SObjectizer's version.
--- dev/so_5/version.rb
+++ dev/so_5/version.rb
@@ -1,3 +1,3 @@
 module So5
-  VERSION = "5.7.4.2"
+  VERSION = "5.7.4.3"
 end
