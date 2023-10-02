1d564429bf8f751595dd4ac631a2da191334732b
Yauheni Akhotnikau <eao197@gmail.com>
2023-10-02 14:38:26 +0300

    Extension of a new unit-test.

--- dev/test/so_5/mbox/sink_binding/bind_then_transform_msg/main.cpp
+++ dev/test/so_5/mbox/sink_binding/bind_then_transform_msg/main.cpp
@@ -180,13 +180,35 @@ ensure_valid_or_die(
 	const std::string_view case_name,
 	const std::string_view expected,
 	const std::string_view actual )
-{
-	ensure_or_die( expected == actual,
-			std::string{ case_name } + ": expected='"
-			+ std::string{ expected }
-			+ "', actual='" + std::string{ actual } + "'" );
-}
-	
+	{
+		ensure_or_die( expected == actual,
+				std::string{ case_name } + ": expected='"
+				+ std::string{ expected }
+				+ "', actual='" + std::string{ actual } + "'" );
+	}
+
+template< typename Msg >
+struct is_implicit_case_applicable_impl_t
+	{
+		static constexpr bool value = true;
+	};
+
+template< typename Msg >
+struct is_implicit_case_applicable_impl_t< so_5::immutable_msg<Msg> >
+	{
+		static constexpr bool value = false;
+	};
+
+template< typename Msg >
+struct is_implicit_case_applicable_impl_t< so_5::mutable_msg<Msg> >
+	{
+		static constexpr bool value = false;
+	};
+
+template< typename Msg >
+constexpr bool is_implicit_case_applicable_v =
+		is_implicit_case_applicable_impl_t<Msg>::value;
+
 struct implicit_type_no_optional_no_dr_t
 	{
 		[[nodiscard]] static std::string_view
@@ -209,6 +231,34 @@ struct implicit_type_no_optional_no_dr_t
 		check_result( std::string_view log )
 			{
 				ensure_valid_or_die( name(), "1-3;2-4;3-5;4-6;", log );
+			}
+	};
+
+struct implicit_type_no_optional_with_dr_t
+	{
+		[[nodiscard]] static std::string_view
+		name() { return { "implicit_type_no_optional_with_dr" }; }
+
+		template< typename Source_Msg, typename Result_Msg, typename Binding >
+		static void
+		tune_binding( Binding & binding, const so_5::mbox_t & from, const so_5::mbox_t & to )
+			{
+				so_5::bind_then_transform(
+						binding,
+						from,
+						[to]( const Source_Msg & src ) {
+							return so_5::make_transformed< Result_Msg >( to,
+									std::to_string( src.m_a ) + "-" + std::to_string( src.m_c ) );
+						},
+						[]( const Source_Msg & src ) {
+							return 1 != src.m_a;
+						} );
+			}
+
+		static void
+		check_result( std::string_view log )
+			{
+				ensure_valid_or_die( name(), "2-4;3-5;4-6;", log );
 			}
 	};
 
@@ -238,6 +288,38 @@ struct implicit_type_with_optional_no_dr_t
 		check_result( std::string_view log )
 			{
 				ensure_valid_or_die( name(), "1-3;2-4;4-6;", log );
+			}
+	};
+
+struct implicit_type_with_optional_with_dr_t
+	{
+		[[nodiscard]] static std::string_view
+		name() { return { "implicit_type_with_optional_with_dr" }; }
+
+		template< typename Source_Msg, typename Result_Msg, typename Binding >
+		static void
+		tune_binding( Binding & binding, const so_5::mbox_t & from, const so_5::mbox_t & to )
+			{
+				using ret_val_t = std::optional< so_5::transformed_message_t< Result_Msg > >;
+				so_5::bind_then_transform(
+						binding,
+						from,
+						[to]( const Source_Msg & src ) -> ret_val_t {
+							if( 3 == src.m_a && 4 == src.m_b && 5 == src.m_c )
+								return std::nullopt;
+
+							return { so_5::make_transformed< Result_Msg >( to,
+									std::to_string( src.m_a ) + "-" + std::to_string( src.m_c ) ) };
+						},
+						[]( const Source_Msg & src ) {
+							return 1 != src.m_a;
+						} );
+			}
+
+		static void
+		check_result( std::string_view log )
+			{
+				ensure_valid_or_die( name(), "2-4;4-6;", log );
 			}
 	};
 
@@ -300,14 +382,26 @@ template< typename Source_Msg, typename Result_Msg >
 void
 run_test_case_for_msg_pair()
 	{
-		run_test_case<
-				so_5::single_sink_binding_t,
-				Source_Msg, Result_Msg,
-				implicit_type_no_optional_no_dr_t >();
-		run_test_case<
-				so_5::single_sink_binding_t,
-				Source_Msg, Result_Msg,
-				implicit_type_with_optional_no_dr_t >();
+		if constexpr( is_implicit_case_applicable_v< Source_Msg > )
+			{
+				run_test_case<
+						so_5::single_sink_binding_t,
+						Source_Msg, Result_Msg,
+						implicit_type_no_optional_no_dr_t >();
+				run_test_case<
+						so_5::single_sink_binding_t,
+						Source_Msg, Result_Msg,
+						implicit_type_no_optional_with_dr_t >();
+				run_test_case<
+						so_5::single_sink_binding_t,
+						Source_Msg, Result_Msg,
+						implicit_type_with_optional_no_dr_t >();
+				run_test_case<
+						so_5::single_sink_binding_t,
+						Source_Msg, Result_Msg,
+						implicit_type_with_optional_with_dr_t >();
+			}
+
 		run_test_case<
 				so_5::multi_sink_binding_t<>,
 				Source_Msg, Result_Msg,
