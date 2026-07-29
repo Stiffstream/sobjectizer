c15b16fd67885d1396c85583125e1f12d69ce1bc
Yauheni Akhotnikau <eao197@gmail.com>
2026-07-29 14:49:58 +0300

    Bump version to 5.8.7.

--- dev/so_5/version.hpp
+++ dev/so_5/version.hpp
@@ -12,7 +12,7 @@
 #if !defined( SO_5_VERSION_HPP )
 #define SO_5_VERSION_HPP
 
-// The current version is 5.8.6
+// The current version is 5.8.7
 //
 /*!
  * A number next to generation number in SObjectizer's version.
@@ -32,7 +32,7 @@
  * When minor version is changed some new features are added to SObjectizer
  * but SObjectizer API is compatible on source-code level.
  */
-#define SO_5_VERSION_MINOR 6ull
+#define SO_5_VERSION_MINOR 7ull
 
 /*!
  * A number next to minor version in SObjectizer's version.
